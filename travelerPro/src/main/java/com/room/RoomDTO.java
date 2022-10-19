24c4ebed5e7a95d38b690801b93825bad1c5962e
jang <jang@DESKTOP-1QOKUOI>
2022-10-19 15:17:23 +0900

    장수인 roomDTO

--- travelerPro/src/main/java/com/room/RoomDTO.java
+++ travelerPro/src/main/java/com/room/RoomDTO.java
@@ -1,5 +1,56 @@
 package com.room;
 
 public class RoomDTO {
-
+	private int roomNum;
+	private String roomName;
+	private String roomInfo;
+	private int price;
+	private int discountRate;
+	private int companyNum;
+	private int headCount;
+	
+	public int getRoomNum() {
+		return roomNum;
+	}
+	public void setRoomNum(int roomNum) {
+		this.roomNum = roomNum;
+	}
+	public String getRoomName() {
+		return roomName;
+	}
+	public void setRoomName(String roomName) {
+		this.roomName = roomName;
+	}
+	public String getRoomInfo() {
+		return roomInfo;
+	}
+	public void setRoomInfo(String roomInfo) {
+		this.roomInfo = roomInfo;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getDiscountRate() {
+		return discountRate;
+	}
+	public void setDiscountRate(int discountRate) {
+		this.discountRate = discountRate;
+	}
+	public int getCompanyNum() {
+		return companyNum;
+	}
+	public void setCompanyNum(int companyNum) {
+		this.companyNum = companyNum;
+	}
+	public int getHeadCount() {
+		return headCount;
+	}
+	public void setHeadCount(int headCount) {
+		this.headCount = headCount;
+	}
+	
+	
 }
