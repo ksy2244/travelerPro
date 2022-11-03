477780d8a06e264a4074d51b06ca8355c77c11ec
ksy2244 <rlathdus2244@naver.com>
2022-11-03 10:36:08 +0900

    마이페이지 숙박 업체 찜 리스트

--- travelerPro/src/main/java/com/mypage/MyPageServlet.java
+++ travelerPro/src/main/java/com/mypage/MyPageServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpSession;
 
 import com.coupon.CouponDTO;
 import com.member.SessionInfo;
+import com.reservation.ReserveCompanyDTO;
 import com.util.TravelServlet;
 import com.util.TravelUtil;
 import com.util.TravelUtilBootstrap;
@@ -21,143 +22,187 @@ import com.util.TravelUtilBootstrap;
 public class MyPageServlet extends TravelServlet {
 	private static final long serialVersionUID = 1L;
 
-	
 	@Override
 	protected void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String uri = req.getRequestURI();
 		req.setCharacterEncoding("utf-8");
-		
+
 		// 세션 정보
 		HttpSession session = req.getSession();
-		SessionInfo info = (SessionInfo)session.getAttribute("member");
-		
-		if(info == null) { // 로그인 되어 있지 않은 경우
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		if (info == null) { // 로그인 되어 있지 않은 경우
 			forward(req, resp, "/WEB-INF/views/member/login.jsp");
 			return;
 		}
-				
-		if(uri.indexOf("coupon.do") != -1) {
+
+		if (uri.indexOf("coupon.do") != -1) {
 			coupon(req, resp);
-		} else if(uri.indexOf("couponInfo.do") != -1) {
+		} else if (uri.indexOf("couponInfo.do") != -1) {
 			couponInfo(req, resp);
-		} if (uri.indexOf("alarm.do") != -1) {
+		} else if (uri.indexOf("alarm.do") != -1) {
 			alarm(req, resp);
+		} else if (uri.indexOf("pick.do") != -1) {
+			pick(req, resp);
 		}
-		
+
 	}
-	
+
 	protected void coupon(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		MypageDAO dao = new MypageDAO();
 		TravelUtil util = new TravelUtilBootstrap();
 		HttpSession session = req.getSession();
-		SessionInfo info = (SessionInfo)session.getAttribute("member");
-		
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
 		String cp = req.getContextPath();
-		
+
 		try {
 			String page = req.getParameter("page");
 			int current_page = 1;
-			if(page != null) {
+			if (page != null) {
 				current_page = Integer.parseInt(page);
 			}
 
 			System.out.print(info.getUserId());
-			
+
 			int dataCount = dao.dataCount(info.getUserId());
-			
+
 			int size = 9;
 			int total_page = util.pageCount(dataCount, size);
-			if(current_page > total_page) {
+			if (current_page > total_page) {
 				current_page = total_page;
 			}
-			
+
 			int offset = (current_page - 1) * size;
-			if(offset < 0) offset = 0;
-			
+			if (offset < 0)
+				offset = 0;
+
 			List<CouponDTO> list = dao.myPageCoupon(info.getUserId(), offset, size);
-			
-			
+
 			String listUrl = cp + "/mypage/coupon.do";
-			String articleUrl = cp + "/mypage/couponInfo.do?page="+current_page;
-			
+			String articleUrl = cp + "/mypage/couponInfo.do?page=" + current_page;
+
 			String paging = util.paging(current_page, total_page, listUrl);
-			
+
 			long gap;
 			Date curDate = new Date();
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			for(CouponDTO dto : list) {
+			for (CouponDTO dto : list) {
 				Date date = sdf.parse(dto.getEnd_date());
-				gap = (date.getTime() - curDate.getTime()) / (1000*60*60*24);
+				gap = (date.getTime() - curDate.getTime()) / (1000 * 60 * 60 * 24);
 				dto.setGap(gap);
-				
+
 				dto.setEnd_date(dto.getEnd_date().substring(0, 10));
 			}
-			
+
 			req.setAttribute("list", list);
 			req.setAttribute("page", current_page);
 			req.setAttribute("dataCount", dataCount);
 			req.setAttribute("articleUrl", articleUrl);
 			req.setAttribute("paging", paging);
-			
-			
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		forward(req, resp, "/WEB-INF/views/mypage/coupon.jsp");
 	}
-	
+
 	protected void couponInfo(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		MypageDAO dao = new MypageDAO();
-		
+
 		String cp = req.getContextPath();
-		
+
 		String page = req.getParameter("page");
-		String query = "page="+page;
-		
+		String query = "page=" + page;
+
 		try {
 			long couponNum = Long.parseLong(req.getParameter("couponNum"));
-			
+
 			HttpSession session = req.getSession();
-			SessionInfo info = (SessionInfo)session.getAttribute("member");
-			
+			SessionInfo info = (SessionInfo) session.getAttribute("member");
+
 			CouponDTO dto = dao.myPageReadCoupon(couponNum, info.getUserId());
-			
-			
-			if(dto == null) {
-				resp.sendRedirect(cp+"/mypage/coupon.do?page=" + page);
+
+			if (dto == null) {
+				resp.sendRedirect(cp + "/mypage/coupon.do?page=" + page);
 				return;
 			}
-			
+
 			dto.setContent(dto.getContent().replaceAll("\r\n", "<br>"));
-			
+
 			long gap;
 			Date curDate = new Date();
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			
+
 			Date date = sdf.parse(dto.getEnd_date());
-			gap = (date.getTime() - curDate.getTime()) / (1000*60*60*24);
+			gap = (date.getTime() - curDate.getTime()) / (1000 * 60 * 60 * 24);
 			dto.setGap(gap);
-				
+
 			dto.setEnd_date(dto.getEnd_date().substring(0, 10));
-			
+
 			req.setAttribute("query", query);
 			req.setAttribute("dto", dto);
 			req.setAttribute("page", page);
-			
+
 			forward(req, resp, "/WEB-INF/views/mypage/couponInfo.jsp");
 			return;
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		resp.sendRedirect(cp+"/mypage/coupon.do?page="+page);
-		
+
+		resp.sendRedirect(cp + "/mypage/coupon.do?page=" + page);
+
 	}
-	
 
 	protected void alarm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-			forward(req, resp, "/WEB-INF/views/mypage/alarm.jsp");
+		forward(req, resp, "/WEB-INF/views/mypage/alarm.jsp");
+	}
+
+	protected void pick(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		MypageDAO dao = new MypageDAO();
+		TravelUtil util = new TravelUtilBootstrap();
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+		String cp = req.getContextPath();
+
+		try {
+			String page = req.getParameter("page");
+			int current_page = 1;
+			if (page != null) {
+				current_page = Integer.parseInt(page);
+			}
+
+			System.out.print(info.getUserId());
+
+			int dataPickCount = dao.dataPickCount(info.getUserId());
+
+			int size = 9;
+			int total_page = util.pageCount(dataPickCount, size);
+			if (current_page > total_page) {
+				current_page = total_page;
+			}
+
+			int offset = (current_page - 1) * size;
+			if (offset < 0)
+				offset = 0;
+
+			List<ReserveCompanyDTO> list = dao.myPickList(info.getUserId(), offset, size);
+
+			String listUrl = cp + "/mypage/pick.do";
+			// String articleUrl = cp + "/mypage/couponInfo.do?page=" + current_page;
+
+			String paging = util.paging(current_page, total_page, listUrl);
+
+			req.setAttribute("list", list);
+			req.setAttribute("page", current_page);
+			req.setAttribute("dataCount", dataPickCount);
+
+			req.setAttribute("paging", paging);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		forward(req, resp, "/WEB-INF/views/mypage/pick.jsp");
 	}
 }
--- travelerPro/src/main/java/com/mypage/MypageDAO.java
+++ travelerPro/src/main/java/com/mypage/MypageDAO.java
@@ -7,38 +7,37 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.coupon.CouponDTO; 
+import com.coupon.CouponDTO;
+import com.reservation.ReserveCompanyDTO;
 import com.util.DBConn;
 
 public class MypageDAO {
 	private Connection conn = DBConn.getConnection();
-	
-	public List<CouponDTO> myPageCoupon(String userId, int offset, int size){
+
+	public List<CouponDTO> myPageCoupon(String userId, int offset, int size) {
 		List<CouponDTO> list = new ArrayList<>();
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String sql;
-		
+
 		try {
 			sql = "SELECT couponNum, couponName, couponRate, couponPrice, content, "
-					+ " TO_CHAR(start_date, 'YYYY-MM-DD') start_date, end_date "
-					+ " FROM coupon "
+					+ " TO_CHAR(start_date, 'YYYY-MM-DD') start_date, end_date " + " FROM coupon "
 					+ " WHERE end_date >= TO_CHAR(SYSDATE, 'YYYY-MM-DD') "
 					+ " AND couponNum NOT IN(SELECT c.couponNum FROM coupon c JOIN myCoupon m ON c.couponNum = m.couponNum WHERE userId = ?) "
-					+ " ORDER BY couponNum DESC "
-					+ " OFFSET ? ROWS FETCH FIRST ? ROWS ONLY ";
-			
+					+ " ORDER BY couponNum DESC " + " OFFSET ? ROWS FETCH FIRST ? ROWS ONLY ";
+
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setString(1, userId);
 			pstmt.setInt(2, offset);
 			pstmt.setInt(3, size);
-			
+
 			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				CouponDTO dto = new CouponDTO();
-				
+
 				dto.setCouponNum(rs.getLong("couponNum"));
 				dto.setCouponName(rs.getString("couponName"));
 				dto.setCouponRate(rs.getInt("couponRate"));
@@ -46,98 +45,96 @@ public class MypageDAO {
 				dto.setContent(rs.getString("content"));
 				dto.setStart_date(rs.getString("start_date"));
 				dto.setEnd_date(rs.getString("end_date"));
-				
+
 				list.add(dto);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if(pstmt != null) {
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (SQLException e) {
 				}
 			}
-			
-			if(rs != null) {
+
+			if (rs != null) {
 				try {
 					pstmt.close();
 				} catch (SQLException e) {
 				}
 			}
 		}
-		
-		
+
 		return list;
-		
+
 	}
-	
+
 	public int dataCount(String userId) {
 		int result = 0;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String sql;
-		
+
 		try {
 			sql = "SELECT COUNT(*) FROM coupon "
 					+ " WHERE TO_CHAR(end_date, 'YYYY-MM-DD') >= TO_CHAR(SYSDATE, 'YYYY-MM-DD') "
 					+ " AND couponNum NOT IN(SELECT c.couponNum FROM coupon c JOIN myCoupon m ON c.couponNum = m.couponNum WHERE userId = ?) ";
-			
+
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setString(1, userId);
-			
+
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				result = rs.getInt(1);
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if(pstmt != null) {
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (SQLException e) {
 				}
 			}
-			
-			if(rs != null) {
+
+			if (rs != null) {
 				try {
 					pstmt.close();
 				} catch (SQLException e) {
 				}
 			}
 		}
-		
+
 		return result;
-		
+
 	}
-	
+
 	public CouponDTO myPageReadCoupon(long couponNum, String userId) {
 		CouponDTO dto = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String sql;
-		
+
 		try {
 			sql = "SELECT couponNum, couponName, couponRate, couponPrice, content, "
-					+ " TO_CHAR(start_date, 'YYYY-MM-DD') start_date, end_date "
-					+ " FROM coupon "
+					+ " TO_CHAR(start_date, 'YYYY-MM-DD') start_date, end_date " + " FROM coupon "
 					+ " WHERE couponNum = ? AND end_date >= TO_CHAR(SYSDATE, 'YYYY-MM-DD') "
 					+ " AND couponNum NOT IN(SELECT c.couponNum FROM coupon c JOIN myCoupon m ON c.couponNum = m.couponNum WHERE userId = ?) ";
-			
+
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setLong(1, couponNum);
 			pstmt.setString(2, userId);
-			
+
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				dto = new CouponDTO();
-				
+
 				dto.setCouponNum(rs.getLong("couponNum"));
 				dto.setCouponName(rs.getString("couponName"));
 				dto.setCouponRate(rs.getInt("couponRate"));
@@ -146,31 +143,120 @@ public class MypageDAO {
 				dto.setStart_date(rs.getString("start_date"));
 				dto.setEnd_date(rs.getString("end_date"));
 			}
-			
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if(pstmt != null) {
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (SQLException e) {
 				}
 			}
-			
-			if(rs != null) {
+
+			if (rs != null) {
 				try {
 					pstmt.close();
 				} catch (SQLException e) {
 				}
 			}
 		}
-		
+
 		return dto;
-		
+
 	}
-	
-	
-	
+
+	public List<ReserveCompanyDTO> myPickList(String userId, int offset, int size) {
+		List<ReserveCompanyDTO> list = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		try {
+			sql = " SELECT c.companyNum, companyName,companyTel, companyInfo " + " FROM pick p  "
+					+ " JOIN company c ON c.companyNum = p.companyNum " + " WHERE p.userId = ? "
+					+ " ORDER BY c.companyNum DESC " + " OFFSET ? ROWS FETCH FIRST ? ROWS ONLY ";
+
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, userId);
+			pstmt.setInt(2, offset);
+			pstmt.setInt(3, size);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+
+				ReserveCompanyDTO dto = new ReserveCompanyDTO();
+
+				dto.setCompanyNum(rs.getInt("companyNum"));
+				dto.setCompanyName(rs.getString("companyName"));
+				dto.setCompanyTel(rs.getString("companyTel"));
+				dto.setCompanyInfo(rs.getString("companyInfo"));
+
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (rs != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return list;
+
+	}
+
+	// 업체 찜 리스트 페이징 처리를 위한 데이터 개수 세기
+	public int dataPickCount(String userId) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+
+		try {
+			sql = " SELECT count(*) FROM pick p  WHERE userId = ? ";
+
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, userId);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				result = rs.getInt(1);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (rs != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return result;
+
+	}
 
 }
